632d830c10bf21c312f9b6c95557fff95fc45690
SANGWOOSEE <washble@devdepot.co.kr>
2024-02-27 01:03:58 +0900

    Create category.html

---
+++ assets/js/list.js
@@ -0,0 +1,14 @@
+const categoryFolderPath = '../img/category'
+const category = [
+    'CATEGORY1',
+    'CATEGORY2',
+    'CATEGORY3',
+    'CATEGORY4',
+    'CATEGORY5',
+];
+
+const listFolderPath = '../views/'
+const list = [
+    'test1',
+    'test2'
+];
