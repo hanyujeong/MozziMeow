cf092fd1d7804a19b9bad1ef60ca144fc54db72d
SANGWOOSEE <washble@devdeopt.co.kr>
2024-04-20 16:41:20 +0900

    CategoryName to Index Card Title

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,7 @@ const categoryCardHTML = (categoryName, imgPath, viewPath) => {
         <img src="${imgPath}" class="bd-placeholder-img card-img-top" width="100%" height="225" alt="Card image cap">
 
         <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text">${categoryName}</p>
             <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
                 <button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='${viewPath}'">View</button>
