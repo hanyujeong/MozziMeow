eda3b308fe51e956f513e25f019f623d93c73340
SANGWOOSEE <washble@devdeopt.co.kr>
2024-04-28 02:17:26 +0900

    Create category Layout

---
+++ assets/js/categoryLayout.js
@@ -0,0 +1,55 @@
+async function executeScript(html) {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = html;
+
+    const scripts = tempDiv.querySelectorAll("script");
+    scripts.forEach(s => s.remove());
+    const contentWithoutScripts = tempDiv.innerHTML;
+
+    for (let script of scripts) {
+        if (script.src) {
+            await new Promise((resolve, reject) => {
+                const newScript = document.createElement('script');
+                newScript.src = script.src;
+                newScript.onload = () => resolve();
+                newScript.onerror = () => reject(new Error(`Script load error for ${script.src}`));
+                document.body.appendChild(newScript);
+            });
+        } else {
+            const newScript = document.createElement('script');
+            newScript.text = script.text;
+            document.body.appendChild(newScript);
+            document.body.removeChild(newScript);
+        }
+    }
+
+    return contentWithoutScripts;
+}
+
+document.addEventListener("DOMContentLoaded", async function() {
+    fetch("../layout/category_head.html")
+        .then(response => response.text())
+        .then(data => {
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = data;
+
+            Array.from(tempDiv.childNodes).forEach(node => {
+                document.head.appendChild(node);
+            });
+        });
+
+    fetch("../layout/category_header.html")
+        .then(response => response.text())
+        .then(async data => {
+            const headerHtml = await executeScript(data);
+            document.getElementById("header-layout").insertAdjacentHTML('beforeend', headerHtml);
+        });
+
+    
+    fetch("../layout/category_footer.html")
+        .then(response => response.text())
+        .then(async data => {
+            const footerHtml = await executeScript(data);
+            document.getElementById("footer-layout").insertAdjacentHTML('beforeend', footerHtml);
+        });
+});
