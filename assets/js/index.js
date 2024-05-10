6a76817b4acea266af1bb037df027197564db60a
SANGWOOSEE <washble@devdepot.co.kr>
2024-05-10 21:13:43 +0900

    add object-fit-cover to category img in index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@ const categoryCardHTML = (categoryName, imgPath, viewPath, num) => {
     `<div class="col">
         <div class="card shadow-sm">
             <label class="custom-cursor" for="view-${num}">
-                <img src="${imgPath}" class="bd-placeholder-img card-img-top" width="100%" height="225" alt="Card image cap">
+                <img src="${imgPath}" class="bd-placeholder-img card-img-top object-fit-cover" width="100%" height="225" alt="Card image cap">
                 <div class="card-body">
                     <div class="my-2">
                         <h3 class="card-text fw-bolder">${categoryName.toUpperCase()}</h3>
